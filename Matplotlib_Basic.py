3eb36972a439cd16c62d6c36346e255e5610914f
KapurV <38396421+kapurvish@users.noreply.github.com>
2019-09-22 23:47:32 +0530

    Update Matplotlib_Basic.py

--- Matplotlib_Basic.py
+++ Matplotlib_Basic.py
@@ -12,16 +12,15 @@ import numpy as nm
 #The color is used to represent the color of the line, here it is red
 plt.plot([2,4,5,6],[4,16,25,36],color='red')
 
-"""
-#Here, 'X' is the label on X axis and 'X*X' is the label on the Y axis,
-#we have specified the font color and size
+#Here, 'X' is the label on X axis and 'X*X' is the label on the Y axis, we have specified the font color and size
+#Output plot present in plot_img.md as Figure_0
 plt.xlabel('X',fontsize=15,color='green')
 plt.ylabel('X*X',fontsize=15,color='green')
 
 
-
 #Here only one list is passed, it is assumed that are the Y coordinates.
 #X coordinates are set to index of each Y coordinate.
+#Output plot present in plot_img.md as Figure_1
 plt.plot([1,5,7,8,9])
 
 
@@ -35,6 +34,7 @@ plt.plot(x_data,nm.sin(x_data),label='sine curve')
 #plt.tick_params(axis='x',bottom=False,labelbottom=False)
 plt.tick_params(axis='y',color='red',labelcolor='blue',labelsize='xx-large')
 #Here we get the legend for the figure.It will pick the label assiciated with the plot function
+#Output plot present in plot_img.md as Figure_2
 plt.legend()
 plt.title('Plot Expereince')
 
@@ -54,10 +54,10 @@ x_data = nm.linspace(start=0,stop=20,num=30)
 plt.plot(x_data,nm.sin(x_data),label='sine curve',color = 'green',linestyle=':', marker= 'd',markersize='10')
 plt.plot(x_data,nm.cos(x_data),label='cos curve',color = 'm')
 plt.legend()
-
+#Output plot present in plot_img.md as Figure_3
 
 fig=plt.figure()
-"""
+  
 
 
 
